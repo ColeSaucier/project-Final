30e52ad865383ec5a9bcffcf2f93bdf3da430d1e
drewlotocki <drewlotocki@gmail.com>
2024-04-12 16:30:31 -0400

    Built some good routes under assinged questions

--- flask-app/src/assigned_questions/assigned_questions.py
+++ flask-app/src/assigned_questions/assigned_questions.py
@@ -29,4 +29,60 @@ def add_new_assignment():
         db.get_db().commit()
 
     return 'Success!'
+
+### Get all the classrooms
+@assigned_questions.route('/classrooms', methods = ['GET'])
+def get_all_categories():
+    query = '''
+        SELECT DISTINCT classId FROM assignedQuestions ORDER BY classId ASC;
+    '''
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+
+    json_data = []
+    # fetch all the column headers and then all the data from the cursor
+    column_headers = [x[0] for x in cursor.description]
+    theData = cursor.fetchall()
+    # zip headers and data together into dictionary and then append to json data dict.
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
     
+    return jsonify(json_data)
+
+### Get the assinment from the classroom
+@assigned_questions.route('/class_assigned', methods = ['GET'])
+def get_selected():
+
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+    query = f'''
+        SELECT * FROM assignedQuestions
+        WHERE classId = '{the_data}'
+    '''
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+
+    json_data = []
+    # fetch all the column headers and then all the data from the cursor
+    column_headers = [x[0] for x in cursor.description]
+    theData = cursor.fetchall()
+    # zip headers and data together into dictionary and then append to json data dict.
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+    
+    return jsonify(json_data)
+
+@assigned_questions.route('/delete_assigned', methods=['DELETE'])
+def delete_assigned():
+    # collecting data from the request object
+    the_data = request.json
+    assignedQuestion = the_data['assignedQuestionId']
+    query = f'DELETE FROM assignedQuestions WHERE assignedQuestionId = {assignedQuestion};'
+    current_app.logger.info(f'Query = {query}')
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    return 'Success!'
+
